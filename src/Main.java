14bfe67eb980a9f85954a614a815fcbc35f470a3
mlstlit <Litvinov.v@mail.ru>
2024-08-10 16:54:20 +0300

    first task

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        byte clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        short creation = 2015;
+
+    }
+}
